232c14661d971f9d8c573cc86e34bb3775a3ff6e
Tobias S <44494591+BruceJi7@users.noreply.github.com>
2021-11-21 22:23:33 +0900

    Started learning-discussion code

--- eventHandlers/events/onReaction/onReaction.go
+++ eventHandlers/events/onReaction/onReaction.go
@@ -3,14 +3,16 @@ package onReaction
 import (
 	"fmt"
 	"tobio/reacto/config"
-	"tobio/reacto/constant"
 	disc "tobio/reacto/discordHelpers"
+	"tobio/reacto/eventHandlers/events/onReaction/reactForRole"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func ParseReactionAdded(s *discordgo.Session, m *discordgo.MessageReactionAdd) {
 	emojiUsed := m.Emoji.MessageFormat()
+
+	fmt.Println(emojiUsed)
 
 	member, err := disc.FetchMember(s, m.UserID)
 	if err != nil {
@@ -22,44 +24,29 @@ func ParseReactionAdded(s *discordgo.Session, m *discordgo.MessageReactionAdd) {
 
 	// If the reaction was on the RFR Post:
 	if m.MessageID == config.RFRPostID {
+		reactForRole.RFRAdd(s, member, emojiUsed)
+	} else {
+		//If not, might be learning-related
+		learningChannel, _ := disc.GetChannelByName(s, "learning-discussion")
+		resourcesChannel, _ := disc.GetChannelByName(s, "learning-resources")
 
-		noLocationRole, _ := disc.GetRoleByName(s, "No-Location")
+		if m.ChannelID == learningChannel.ID && emojiUsed == "💡" {
 
-		//If the role matches one of the RFR roles
-		if RFRRoleSelected, exists := constant.RFRMap[emojiUsed]; exists {
-
-			role, err := disc.GetRoleByName(s, RFRRoleSelected)
+			message, err := s.ChannelMessage(learningChannel.ID, m.MessageID)
 			if err != nil {
-				fmt.Println("Whilst parsing reaction added:")
-				fmt.Println("Error finding role")
+				fmt.Println("Whilst parsing reaction added")
+				fmt.Println("Whilst handling learning-discussion reaction")
+				fmt.Println("Error finding message")
 				fmt.Println(err)
 				return
 			}
-
-			for _, userExistingRoleID := range member.Roles {
-				if userExistingRoleID == role.ID {
-					// They already have the role, EJECT
-					return
-				}
-			}
-			s.GuildMemberRoleAdd(config.GuildID, member.User.ID, role.ID)
-			logMessage := fmt.Sprintf("%s User %s receives role %s", disc.Log.RoleAdded, member.User.Username, RFRRoleSelected)
-			disc.SendLog(s, logMessage)
+			fmt.Println(resourcesChannel.ID)
+			fmt.Println(message.ChannelID)
+			// TODO - count the amount of reactions
+			// If over 5, copy the message over to learning-resources
 
 		}
 
-		// Check if they have the no-location role, and remove it
-		for _, userExistingRoleID := range member.Roles {
-			if userExistingRoleID == noLocationRole.ID {
-				err = s.GuildMemberRoleRemove(config.GuildID, member.User.ID, noLocationRole.ID)
-				if err != nil {
-					fmt.Println("Whilst parsing reaction removed:")
-					fmt.Println("Error removing no-location role")
-					fmt.Println(err)
-				}
-
-			}
-		}
 	}
 
 }
@@ -77,81 +64,6 @@ func ParseReactionRemoved(s *discordgo.Session, m *discordgo.MessageReactionRemo
 
 	// If the reaction was on the RFR Post:
 	if m.MessageID == config.RFRPostID {
-
-		noLocationRole, _ := disc.GetRoleByName(s, "No-Location")
-
-		// If the role matches one of the RFR roles
-		// RFRRoleSelected == role that the reaction was for
-		if RFRRoleSelected, exists := constant.RFRMap[emojiUsed]; exists {
-
-			// Get full role object for RFR role used
-			role, err := disc.GetRoleByName(s, RFRRoleSelected)
-			if err != nil {
-				fmt.Println("Whilst parsing reaction removed:")
-				fmt.Println("Error finding role")
-				fmt.Println(err)
-				return
-			}
-
-			// If the user actually has that role, remove it.
-			shouldRemove := false
-			for _, userExistingRoleID := range member.Roles {
-				if userExistingRoleID == role.ID {
-					shouldRemove = true
-					break
-				}
-			}
-
-			if shouldRemove {
-
-				err = s.GuildMemberRoleRemove(config.GuildID, member.User.ID, role.ID)
-				if err != nil {
-					fmt.Println("Whilst parsing reaction removed:")
-					fmt.Println("Error removing role")
-					fmt.Println(err)
-					return
-				}
-				fmt.Println("Successfully removed ", RFRRoleSelected)
-				logMessage := fmt.Sprintf("%s User %s loses role %s", disc.Log.RoleAdded, member.User.Username, RFRRoleSelected)
-				disc.SendLog(s, logMessage)
-			}
-
-			// If the user has none of the RFR roles, give them 'No-Location'
-
-			shouldAddNoLocation := true
-			for _, usersRoleID := range member.Roles { // Check over the roles the user has
-
-				roleUserHas, _ := disc.GetRoleByID(s, usersRoleID)
-
-				if roleUserHas.Name == RFRRoleSelected {
-
-					continue
-				} else {
-
-					// Scan the list of RFR roles for this role
-					for _, RFRRole := range constant.RFRRoles {
-						// If RFR list contains the current role we're checking for
-						if RFRRole == roleUserHas.Name {
-							shouldAddNoLocation = false
-							break
-						}
-					}
-
-					if !shouldAddNoLocation {
-						break
-					}
-				}
-			}
-			// If none of the location-based (RFR) roles
-			// Add No-location role
-			if shouldAddNoLocation {
-				logMessage := fmt.Sprintf("%s User %s has no location-based roles, gains No-Location", disc.Log.RoleAdded, member.User.Username)
-				disc.SendLog(s, logMessage)
-				fmt.Println("Add no location")
-				s.GuildMemberRoleAdd(config.GuildID, member.User.ID, noLocationRole.ID)
-			}
-
-		}
-
+		reactForRole.RFRRemove(s, member, emojiUsed)
 	}
 }
